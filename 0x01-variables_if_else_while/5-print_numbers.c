d2df4a73eb8b68880c211665610965b65d4cf311
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-06 14:26:33 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: is zero
+ */
+
+int main(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}

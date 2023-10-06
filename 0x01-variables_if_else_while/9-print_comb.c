4e72959783579d74239eceee9257225d10ffcbe4
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-06 16:52:22 -0700

    Patience, persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: is zero
+ */
+
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

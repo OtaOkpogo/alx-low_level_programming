7b9dd21967e469c44a95fd24cbcefe3918bbaf33
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-11 10:55:15 -0700

    Task 12 Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,10 @@
 
 void print_times_table(int n)
 {
-if (n < 0 || n > 15)
-{
-
 int i, j, k;
+
+if (n < 0 || n > 15)
+return;
 
 for (i = 0; i <= n; i++)
 {
@@ -25,20 +25,17 @@ _putchar(',');
 _putchar(' ');
 
 if (k < 10)
-{
+
 _putchar(' ');
-_putchar(' ');
-}
-else if (k < 100)
-{
+
+if (k < 100)
 _putchar(' ');
 }
-}
+
 _putchar(k / 100 + '0');
 _putchar((k / 10) % 10 + '0');
 _putchar(k % 10 + '0');
 }
 _putchar('\n');
-}
 }
 }

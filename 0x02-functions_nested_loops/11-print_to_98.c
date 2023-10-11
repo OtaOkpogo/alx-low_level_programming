30cbacd62c4de3fe02edbd4575e1485718a93146
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-11 01:32:09 -0700

    Task 11, 98 battery

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - This Prints all natural numbers from n to 98, followed by a new line.
+ * @n: The starting number.
+ * return: void
+ */
+
+void print_to_98(int n)
+
+{
+int i;
+
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+}
+printf("\n");
+}

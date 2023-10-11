cb9463e08fa704d97b4e299e61cf155617e57fc1
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-11 11:53:51 -0700

    In computer class

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,24 +6,39 @@
  */
 
 int main(void)
-
 {
-unsigned int a = 1, b = 2, next;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-printf("%u, %u, ", a, b);
+	printf("%lu", bef);
 
-for (unsigned int i = 3; i <= 98; i++)
-{
-next = a + b;
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
 
-if (i < 98)
-printf("%u, ", next);
-else
-printf("%u\n", next);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
-a = b;
-b = next;
-}
-
-return (0);
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
 }

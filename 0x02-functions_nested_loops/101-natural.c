1a1e656b632cd84a6e52cf5c3a3e797cd4af0c03
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-11 11:05:48 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,8 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - This is the function that lists all the natural numbers below 1024 (excluded)
- *        that are multiples of 3 or 5.
+ * main - This is the function that lists all the natural numbers
  *
  * Return: Always 0.
  */

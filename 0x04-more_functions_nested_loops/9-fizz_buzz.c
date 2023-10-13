40a25a560f799d3ce31e9168c305d91d3fd99222
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-12 19:21:22 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int p;
+
+for (p = 1; p <= 100; p++)
+{
+if (p % 3 == 0 && p % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (p % 5 == 0 && p % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (p % 3 == 0 && p % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (p == 1)
+{
+printf("%d", p);
+}
+else
+{
+printf(" %d", p);
+}
+}
+printf("\n");
+
+return (0);
+}
+

79f7bd8fc876edd2daa55eb8206a969ca1af1467
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-16 12:02:01 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,12 @@
 void rev_string(char *s)
 {
 int length = 0;
+int i;
 while (s[length] != '\0')
 {
 length++;
 }
-for (int i = 0; i < length / 2; i++)
+for (i = 0; i < length / 2; i++)
 {
 char temp = s[i];
 s[i] = s[length - 1 - i];

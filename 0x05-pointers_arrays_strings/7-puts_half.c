f6c908bbb75b9800368b26fed482fb46e85ae272
OtaOkpogo <otaokpogo@yahoo.com>
2023-10-16 12:48:49 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - this Print the second half of a string.
+ * @str: Input string.
+ * Return: This function doesn't return a value.
+ */
+void puts_half(char *str)
+{
+int a, n, length;
+
+length = 0;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+length++;
+}
+n = length / 2;
+if (length % 2 == 1)
+{
+n = (length + 1) / 2;
+}
+
+for (a = n; str[a] != '\0'; a++)
+{
+_putchar(str[a]);
+}
+_putchar('\n');
+}

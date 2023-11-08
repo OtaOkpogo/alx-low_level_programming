ada05b9c4ed8e3282706261828bf916ff6bd650e
OtaOkpogo <otaokpogo@yahoo.com>
2023-11-08 02:39:15 -0800

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,25 +1,39 @@
-#include "function_pointers.h"
+#include <stdio.h>
+#include "0-print_name.h"
+/**
+ * print_to_stdout - Prints a name to the standard output
+ * @name: The name to be printed
+ */
+void print_to_stdout(char *name)
+{
+if (name != NULL)
+{
+printf("Name: %s\n", name);
+}
+}
 
 /**
-* array_iterator - a function given as a
-* parameter on each element of an array.
-* @array: array to execute func on
-* @size: is the size of the array
-* @action:  is a pointer to the function you need to use
-* Return: void
-*/
-void array_iterator(int *array, size_t size, void (*action)(int))
+ * print_name - Calls a function to print a name
+ * @name: The name to be printed
+ * @f: A function pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
 {
-size_t a;
-
-
-if (array && action)
+if (name != NULL && f != NULL)
 {
-a = 0;
-while (a < size)
-{
-action(array[a]);
-a++;
+f(name);
 }
 }
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char myName[] = "John";
+print_name(myName, print_to_stdout);
+
+return (0);
 }
---
+++ 0x0F-function_pointers/0-print_name.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_NAME_H
+#define PRINT_NAME_H
+
+void print_to_stdout(char *name);
+void print_name(char *name, void (*f)(char *));
+
+#endif

7de20f5bff6b54133aafa1ca889ddbd695179aba
OtaOkpogo <otaokpogo@yahoo.com>
2023-11-09 17:30:36 -0800

    one womans constant

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,19 +14,20 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int x;
-va_list ap;
-va_start(ap, n);
+char *str;
 
-if (separator == NULL)
-separator = "";
+va_list ap;
+
+va_start(ap, n);
 
 for (x = 0; x < n; x++)
 {
-char *str = va_arg(ap, char *);
-if (str != NULL)
-printf("%s", str);
-
-if (x < n - 1)
+str = va_arg(ap, char *);
+if (str == NULL)
+printf("(nil)");
+else
+	printf("%s", str);
+if (x != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 
